1e32a1ac76df8e1060a376c2fad3c3b84f11f440
Hassan Syed <hassantsyed@gmail.com>
2023-10-07 23:07:27 -0400

    dkglj

--- src/app.ts
+++ src/app.ts
@@ -63,8 +63,6 @@ app.webhooks.onError((error) => {
   }
 });
 
-console.log(process.env);
-
 const port = process.env.PORT || 3000;
 const host = process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost';
 
