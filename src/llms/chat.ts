ebb37393349e6aa71641a64ba171a7e4da71e29e
Faizan Ahmed <43652410+faizancodes@users.noreply.github.com>
2024-11-20 15:50:22 -0500

    Update chat.ts

--- src/llms/chat.ts
+++ src/llms/chat.ts
@@ -2,12 +2,10 @@ import { ChatCompletionCreateParamsNonStreaming } from "groq-sdk/resources/chat/
 import { groq, GROQ_MODEL } from "./groq";
 
 export const generateChatCompletion = async (
-  options: Omit<ChatCompletionCreateParamsNonStreaming, "model">
+  options: <ChatCompletionCreateParamsNonStreaming, "model">
 ) => {
   const response = await groq.chat.completions.create({
     model: GROQ_MODEL,
-    temperature: 0,
-    ...options,
   });
   return response.choices[0].message;
 };
